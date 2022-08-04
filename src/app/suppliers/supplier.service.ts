27bbab1bef385c824a73fc9de4a72524128d0cc6
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2022-08-04 17:19:53 +0200

    Concat, Merge and Switch Map

--- src/app/suppliers/supplier.service.ts
+++ src/app/suppliers/supplier.service.ts
@@ -1,7 +1,9 @@
+/* eslint-disable rxjs/no-nested-subscribe */
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
-import { throwError, Observable, of, map } from 'rxjs';
+import { throwError, Observable, of, map, concatMap, tap, mergeMap, switchMap } from 'rxjs';
+import { Supplier } from './supplier';
 
 @Injectable({
   providedIn: 'root',
@@ -10,10 +12,29 @@ export class SupplierService {
   suppliersUrl = 'api/suppliers';
 
   constructor(private http: HttpClient) {
-    this.ho$.subscribe((x) => console.log(x));
+    this.supplierWithConcatMap$.subscribe((x) => console.log(`Supplier for concatMap `, x));
+    this.supplierWithMergeMap$.subscribe((x) => console.log(`Supplier for mergeMap `, x));
+    this.supplierWithSwitchMap$.subscribe((x) => console.log(`Supplier for switchMap `, x));
   }
 
-  ho$ = of(2, 5, 8).pipe(map((value) => of(`inner obs value (${value})`)));
+  // supplierWithMap$ = of(1, 5, 8).pipe(
+  //   map((id) => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+  // );
+
+  supplierWithConcatMap$ = of(1, 5, 8).pipe(
+    tap((value) => console.log(`emitted from concatMap source obs (${value})`)),
+    concatMap((id) => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+  );
+
+  supplierWithMergeMap$ = of(1, 5, 8).pipe(
+    tap((value) => console.log(`emitted from mergeMap source obs (${value})`)),
+    mergeMap((id) => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+  );
+
+  supplierWithSwitchMap$ = of(1, 5, 8).pipe(
+    tap((value) => console.log(`emitted from switchMap source obs (${value})`)),
+    switchMap((id) => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+  );
 
   private handleError(err: HttpErrorResponse): Observable<never> {
     // in a real world app, we may send the server to some remote logging infrastructure
